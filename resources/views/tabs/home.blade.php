82ca960aaa3025051dd1ec234278eef3633308cc
ZeiteRoN <zeiter1007@gmail.com>
2024-04-05 19:53:45 +0300

    Added Calendar

--- resources/views/tabs/home.blade.php
+++ resources/views/tabs/home.blade.php
@@ -4,4 +4,4 @@
 	<div class="js-order w-1/2 h-40 bg-red-300 rounded-lg">Order</div>
 </div>
 <div class="js-feedbacks">@include('tabs._elements.feedback_field')</div>
-<div class="js-calendar">Calendar</div>
+<div class="js-calendar">@include('tabs._elements.calendar')</div>
